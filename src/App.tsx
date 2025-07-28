40310e1c24d475193b7de22a37aba30fc41ee1ec
sn0wstorm20202 <somsubhra20202@gmail.com>
2025-07-28 22:45:37 +0530

    Database Add on

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Builder from "./pages/Builder";
 import NotFound from "./pages/NotFound";
+import Testpage from "./pages/Testpage";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,8 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/builder" element={<Builder />} />
+          <Route path="/test" element={<Testpage />} />
+          
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
---
+++ src/pages/Testpage.tsx
@@ -0,0 +1,9 @@
+const Testpage = () => {
+    return (
+        <div>
+            <h1>Testpage</h1>
+        </div>
+    );
+}
+
+export default Testpage;
